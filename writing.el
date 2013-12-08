2cae0091a9728d789791e6c2e223a67e6d0e2956
Daniil Frumin <difrumin@gmail.com>
2013-12-08 17:48:43 +0400

    LaTeX settings

--- writing.el
+++ writing.el
@@ -8,7 +8,7 @@
 
 (defun make-markdown-look-better ()
   (interactive)
-  (text-scale-increase 1)
+;;  (text-scale-increase 1)
   (linum-mode -1)
   (set-window-margins nil 2 1))
 
@@ -27,3 +27,41 @@
         ("Preview" "open -a Preview.app %o")))
 
 (setq TeX-view-program-selection '((output-pdf "Preview")))
+
+(defun fix-latex-cmd () 
+  (setf (cadr (assoc "LaTeX" TeX-command-list)) "%`%l%(mode) -file-line-error %' %t"))
+
+(require 'reftex)
+
+(add-hook 'LaTeX-mode-hook 'fix-latex-cmd)
+(add-hook 'LaTeX-mode-hook 'reftex-mode)
+(add-hook 'LaTeX-mode-hook 'outline-minor-mode)
+
+(add-to-list 'reftex-default-bibliography
+             "/Users/dan/Dropbox/Public/t/main")
+
+(add-to-list 'reftex-bibpath-environment-variables
+             "/Users/dan/Dropbox/Public/t/")
+(setq reftex-external-file-finders
+      '(("tex" . "/usr/texbin/kpsewhich -format=.tex %f")
+  ("bib" . "/usr/texbin/kpsewhich -format=.bib %f")))
+
+(setq TeX-outline-extra
+      '(("%%%" 3)
+        ("%%%%" 6)))
+
+(setq reftex-plug-into-AUCTeX t)
+
+(eval-after-load "preview"
+  '(add-to-list 'preview-default-preamble "\\PreviewEnvironment{tikzpicture}" t)
+  )
+
+;; ORG writing
+(setq org-export-with-smart-quotes t)
+
+(setq outline-minor-mode-hook '())
+(add-hook 'outline-minor-mode-hook
+          (lambda ()
+            (require 'outline-magic)
+            (define-key outline-minor-mode-map [(f9)] 'outline-cycle)))
+
